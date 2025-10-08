cdb8e8d83946e609394f451efc0d6bad201c72b3
David Horès <hores.d@gmail.com>
2025-10-08 19:12:38 +0200

    Add HTML structure and header to index.php and define global and header styles in style.css

---
+++ public/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Accueil - Tom Troc</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <img src="images/tom-troc-logo.svg" alt="Logo Tom Troc">
+            <nav>
+                <ul class="nav-list">
+                    <li>
+                        <a href="#" class="nav-link-active">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="#">Nos livres à l'échange</a>
+                    </li>
+                </ul>
+                <ul class="nav-list nav-list-user">
+                    <li>
+                        <img src="images/message-icon.svg" alt="Icône Messagerie" class="nav-icon">
+                        <a href="#">Messagerie</a>
+                        <span class="message-counter" aria-label="1 message non lu">1</span>
+                    </li>
+                    <li>
+                        <img src="images/account-icon.svg" alt="Icône Mon compte" class="nav-icon">
+                        <a href="#">Mon compte</a>
+                    </li>
+                    <li>
+                        <a href="#">Connexion</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main></main>
+        <footer></footer>        
+    </body>
+</html>
