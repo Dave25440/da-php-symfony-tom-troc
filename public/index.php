97a8b69071c12f8fb4e53bb65af5aa5ceb212999
David Horès <hores.d@gmail.com>
2025-10-09 12:13:12 +0200

    Add join section to index.php and update style

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="css/style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     </head>
     <body>
         <header>
@@ -25,7 +25,7 @@
                     <li>
                         <img src="images/message-icon.svg" alt="Icône Messagerie" class="nav-icon">
                         <a href="#">Messagerie</a>
-                        <span class="message-counter" aria-label="1 message non lu">1</span>
+                        <span class="nav-message-counter" aria-label="1 message non lu">1</span>
                     </li>
                     <li>
                         <img src="images/account-icon.svg" alt="Icône Mon compte" class="nav-icon">
@@ -37,12 +37,27 @@
                 </ul>
             </nav>
         </header>
-        <main></main>
+        <main>
+            <section class="section-join">
+                <div class="section-join-content">
+                    <h1 class="title">Rejoignez nos lecteurs passionnés</h1>
+                    <p class="section-join-text text-light">Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
+                    <a href="#" class="cta">Découvrir</a>
+                </div>
+                <figure>
+                    <img src="images/reading-hamza-nouasria.webp" alt="Homme en train de lire assis au milieu de piles de livres">
+                    <figcaption>Hamza</figcaption>
+                </figure>
+            </section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </main>
         <footer>
             <ul class="footer-list">
-                <li><a href="#" class="footer-text">Politique de confidentialité</a></li>
-                <li><a href="#" class="footer-text">Mentions légales</a></li>
-                <li class="footer-text">Tom Troc©</li>
+                <li><a href="#" class="text-light">Politique de confidentialité</a></li>
+                <li><a href="#" class="text-light">Mentions légales</a></li>
+                <li class="text-light">Tom Troc©</li>
                 <li><img src="images/tt-logo.svg" alt="Logo TT"></li>
             </ul>
         </footer>        
