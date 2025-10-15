e7eca121fe982810dcf5b09c2fa3baab1ecfd3cd
David Horès <hores.d@gmail.com>
2025-10-15 18:54:20 +0200

    Update books section in books.php and add images to project

--- public/books.php
+++ public/books.php
@@ -103,6 +103,150 @@
                             </div>
                         </article>
                     </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/delight-rossow.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Delight!</h3>
+                                <h4>Justin Rossow</h4>
+                                <p class="text-caption">Vendu par : Juju1432</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/milwaukee-mission-low.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Milwaukee Mission</h3>
+                                <h4>Elder Cooper Low</h4>
+                                <p class="text-caption">Vendu par : Christiane75014</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/minimalist-graphics-schonlau.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Minimalist Graphics</h3>
+                                <h4>Julia Schonlau</h4>
+                                <p class="text-caption">Vendu par : Hamzalecture</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/hygge-wiking.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Hygge</h3>
+                                <h4>Meik Wiking</h4>
+                                <p class="text-caption">Vendu par : Hugo1990_12</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/innovation-ridley.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Innovation</h3>
+                                <h4>Matt Ridley</h4>
+                                <p class="text-caption">Vendu par : Lou&Ben50</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/psalms-alabaster.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Psalms</h3>
+                                <h4>Alabaster</h4>
+                                <p class="text-caption">Vendu par : Lolobzh</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/thinking-fast-kahneman.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Thinking, Fast & Slow</h3>
+                                <h4>Daniel Kahneman</h4>
+                                <p class="text-caption">Vendu par : Sas634</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/book-hope-kaur.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>A Book Full of Hope</h3>
+                                <h4>Rupi Kaur</h4>
+                                <p class="text-caption">Vendu par : ML95</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/subtle-art-manson.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>The Subtle Art of Not Giving a F*ck</h3>
+                                <h4>Mark Manson</h4>
+                                <p class="text-caption">Vendu par : Verogo33</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/narnia-lewis.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Narnia</h3>
+                                <h4>C.S. Lewis</h4>
+                                <p class="text-caption">Vendu par : AnnikaBrahms</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/company-one-jarvis.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>Company of One</h3>
+                                <h4>Paul Jarvis</h4>
+                                <p class="text-caption">Vendu par : Victoirefabr912</p>
+                            </div>
+                        </article>
+                    </a>
+                    <a href="#" class="book-card">
+                        <article>
+                            <figure>
+                                <img src="images/two-towers-tolkien.webp" alt="">
+                            </figure>
+                            <div class="book-info">
+                                <h3>The Two Towers</h3>
+                                <h4>J.R.R. Tolkien</h4>
+                                <p class="text-caption">Vendu par : Lotrfanclub67</p>
+                            </div>
+                        </article>
+                    </a>
                 </div>
             </section>
         </main>
