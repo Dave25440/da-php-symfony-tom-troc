2a0cfcf5d3cbeec5655ea3330fede877b1e58c84
David Horès <hores.d@gmail.com>
2025-10-15 18:38:30 +0200

    Create books.php, add books section to it and update style and index.php

--- public/books.php
+++ public/books.php
@@ -21,10 +21,10 @@
             <nav>
                 <ul>
                     <li class="nav-item-left">
-                        <a href="index.php" class="active">Accueil</a>
+                        <a href="index.php">Accueil</a>
                     </li>
                     <li class="nav-item-left">
-                        <a href="books.php">Nos livres à l'échange</a>
+                        <a href="books.php" class="active">Nos livres à l'échange</a>
                     </li>
                     <li class="nav-item-right nav-item-border">
                         <img src="icons/message-icon.svg" alt="Icône Messagerie" class="nav-icon">
@@ -45,19 +45,15 @@
             </nav>
         </header>
         <main>
-            <section class="section-join">
-                <div class="section-join-content">
-                    <h1 class="title">Rejoignez nos lecteurs passionnés</h1>
-                    <p class="text-light">Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
-                    <a href="#" class="cta">Découvrir</a>
-                </div>
-                <figure>
-                    <img src="images/reading-hamza-nouasria.webp" alt="Homme en train de lire assis au milieu de piles de livres">
-                    <figcaption class="text-caption">Hamza</figcaption>
-                </figure>
-            </section>
-            <section class="section-books-preview">
-                <h2 class="title">Les derniers livres ajoutés</h2>
+            <section class="section-books">
+                <header class="section-books-header">
+                    <h1 class="title">Nos livres à l’échange</h1>
+                    <form role="search">
+                        <img src="icons/search-icon.svg" alt="" aria-hidden=true>
+                        <label for="search-input" class="sr-only">Rechercher un livre</label>
+                        <input type="search" id="search-input" placeholder="Rechercher un livre">
+                    </form>
+                </header>
                 <div class="books-grid">
                     <a href="#" class="book-card">
                         <article>
@@ -99,38 +95,6 @@
                             </div>
                         </article>
                     </a>
-                </div>
-                <a href="books.php" class="cta">Voir tous les livres</a>
-            </section>
-            <section class="section-how-it-works">
-                <h2 class="title">Comment ça marche ?</h2>
-                <p class="text-light">
-                    Échanger des livres avec TomTroc c’est simple et<br>
-                    amusant ! Suivez ces étapes pour commencer :
-                </p>
-                <ol>
-                    <li>Inscrivez-vous gratuitement sur notre plateforme.</li>
-                    <li>Ajoutez les livres que vous souhaitez échanger à votre profil.</li>
-                    <li>Parcourez les livres disponibles chez d'autres membres.</li>
-                    <li>Proposez un échange et discutez avec d'autres passionnés de lecture.</li>
-                </ol>
-                <a href="books.php" class="cta cta-reverse">Voir tous les livres</a>
-            </section>
-            <section class="section-values">
-                <figure class="section-values-banner">
-                    <img src="images/banner-darwin-vegher.webp" alt="" aria-hidden="true">
-                </figure>
-                <div class="section-values-content">
-                    <h2 class="title">Nos valeurs</h2>
-                    <div class="section-values-text text-light">
-                        <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
-                        <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.</p>
-                        <p>Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
-                    </div>
-                    <footer class="section-values-footer">
-                        <p class="text-caption">L'équipe Tom Troc</p>
-                        <img src="images/tom-troc-signature.svg" alt="" aria-hidden="true">
-                    </footer>
                 </div>
             </section>
         </main>
