5034d2f27c12e56ebfec898463715a5079930ac3
David Horès <hores.d@gmail.com>
2025-10-16 16:13:41 +0200

    Create book.php, add breadcrumb nav to it and update style

---
+++ public/book.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>The Kinfolk Table - Tom Troc</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+        <script src="js/menu-toggle.js" defer></script>
+    </head>
+    <body>
+        <header class="page-header">
+            <div class="header-brand">
+                <img src="images/tom-troc-logo.svg" alt="Logo Tom Troc">
+                <button id="menu-toggle" aria-label="Afficher le menu" type="button">
+                    <img src="icons/menu-icon.svg" alt="">
+                </button>
+            </div>
+            <nav class="main-nav">
+                <ul>
+                    <li class="nav-item-left">
+                        <a href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item-left">
+                        <a href="books.php" class="active">Nos livres à l'échange</a>
+                    </li>
+                    <li class="nav-item-right nav-item-border">
+                        <img src="icons/message-icon.svg" alt="Icône Messagerie" class="nav-icon">
+                        <a href="message.php">Messagerie</a>
+                        <span class="nav-counter" aria-live="polite">
+                            1
+                            <span class="sr-only"> message(s) non lu(s)</span>
+                        </span>
+                    </li>
+                    <li class="nav-item-right">
+                        <img src="icons/account-icon.svg" alt="Icône Mon compte" class="nav-icon">
+                        <a href="#">Mon compte</a>
+                    </li>
+                    <li class="nav-item-right">
+                        <a href="#">Connexion</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <nav class="breadcrumb-nav">
+                <ol class="text-light">
+                    <li>
+                        <a href="books.php">Nos livres</a>
+                    </li>
+                    <li>
+                        <a href="book.php">The Kinfolk Table</a>
+                    </li>
+                </ol>
+            </nav>
+        </main>
+        <footer class="page-footer text-light">
+            <ul>
+                <li><a href="#">Politique de confidentialité</a></li>
+                <li><a href="#">Mentions légales</a></li>
+                <li>Tom Troc©</li>
+                <li><img src="images/tt-logo.svg" alt="Logo TT"></li>
+            </ul>
+        </footer>
+    </body>
+</html>
