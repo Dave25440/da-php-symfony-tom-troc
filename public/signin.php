250efe8a0fda2d652c70bfdc44479dd508713263
David Horès <hores.d@gmail.com>
2025-10-20 16:28:05 +0200

    Add form-label class to global style and update signin.php and style of signin section in style.css

--- public/signin.php
+++ public/signin.php
@@ -46,11 +46,24 @@
         </header>
         <main>
             <section class="section-signin">
-                <div></div>
+                <div class="section-signin-wrapper">
+                    <h1 class="title">Connexion</h1>
+                    <form aria-label="Formulaire de connexion">
+                        <label for="email" class="form-label">Adresse email</label>
+                        <input type="email" id="email" class="form-input">
+                        <label for="password" class="form-label">Mot de passe</label>
+                        <input type="password" id="password" class="form-input">
+                        <input type="submit" id="signin" value="Se connecter" class="cta cta-input">
+                    </form>
+                    <p>
+                        Pas de compte ?
+                        <a href="signup.php">Inscrivez-vous</a>
+                    </p>
+                </div>
                 <figure>
                     <img src="images/library-marialaura-gionfriddo.webp" alt="Étagères d'une bibliothèque remplies de piles de livres" class="img-cover">
                 </figure>
-            </article>
+            </section>
         </main>
         <footer class="page-footer text-light">
             <ul>
