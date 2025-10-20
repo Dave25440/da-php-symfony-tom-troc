f54b4cda43968dddd442976b877a0c9fe14ce22e
David Horès <hores.d@gmail.com>
2025-10-20 19:46:54 +0200

    Add title-uppercase class to global style and update book.php and style.css

--- public/book.php
+++ public/book.php
@@ -63,14 +63,14 @@
                     <h1 class="title">The Kinfolk Table</h1>
                     <h2>par Nathan Williams</h2>
                     <hr>
-                    <h3>Description</h3>
+                    <h3 class="title-uppercase">Description</h3>
                     <div class="article-book-text">
                         <p>J'ai récemment plongé dans les pages de 'The Kinfolk Table' et j'ai été enchanté par cette œuvre captivante. Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager des moments authentiques autour de la table.</p>
                         <p>Les photographies magnifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans un voyage à travers des recettes et des histoires qui mettent en avant la beauté de la simplicité et de la convivialité.</p>
                         <p>Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres chers.</p>
                         <p>'The Kinfolk Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain que ce livre trouvera une place spéciale dans le cœur de tout amoureux de la cuisine et des rencontres inspirantes.</p>
                     </div>
-                    <h3>Propriétaire</h3>
+                    <h3 class="title-uppercase">Propriétaire</h3>
                     <a href="profile.php" class="user-card">
                         <figure>
                             <img src="images/user-nathalire.webp" alt="" class="img-cover">
