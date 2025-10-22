7bd1f276c8f5ca7469632ec7fcf4237411f3fa6b
David Horès <hores.d@gmail.com>
2025-10-22 18:50:01 +0200

    Create edit.php, add edit section to it and update style

---
+++ public/edit.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Éditer The Kinfolk Table - Tom Troc</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+        <script src="js/menu-toggle.js" defer></script>
+    </head>
+    <body>
+        <header class="page-header">
+            <div class="header-brand">
+                <img src="images/tom-troc-logo.svg" alt="Logo Tom Troc">
+                <button id="menu-toggle" aria-label="Afficher le menu" type="button">
+                    <img src="icons/menu-icon.svg" alt="">
+                </button>
+            </div>
+            <nav class="main-nav">
+                <ul>
+                    <li class="nav-item-left">
+                        <a href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item-left">
+                        <a href="books.php">Nos livres à l'échange</a>
+                    </li>
+                    <li class="nav-item-right nav-item-border">
+                        <img src="icons/message-icon.svg" alt="Icône Messagerie" class="nav-icon">
+                        <a href="message.php">Messagerie</a>
+                        <span class="nav-counter" aria-live="polite">
+                            1
+                            <span class="sr-only"> message(s) non lu(s)</span>
+                        </span>
+                    </li>
+                    <li class="nav-item-right">
+                        <img src="icons/account-icon.svg" alt="Icône Mon compte" class="nav-icon">
+                        <a href="account.php" class="active">Mon compte</a>
+                    </li>
+                    <li class="nav-item-right">
+                        <a href="signin.php">Connexion</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section class="section-edit">
+                <a href="account.php"><- retour</a>
+                <h1 class="title">Modifier les informations</h1>
+                <div class="section-edit-wrapper">
+                    <figure>
+                        <figcaption class="form-label">Photo</figcaption>
+                        <img src="images/kinfolk-table-williams.webp" alt="" class="img-cover">
+                        <a href="#">Modifier la photo</a>
+                    </figure>
+                    <form>
+                        <label for="title" class="form-label">Titre</label>
+                        <input type="text" id="title" name="title" value="The Kinfolk Table" class="form-input form-input-update">
+                        <label for="author" class="form-label">Auteur</label>
+                        <input type="text" id="author" name="author" value="Nathan Williams" class="form-input form-input-update">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea id="description" name="description" rows="19" class="form-input form-input-update">
+J'ai récemment plongé dans les pages de 'The Kinfolk Table' et j'ai été enchanté par cette œuvre captivante. Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager des moments authentiques autour de la table.
+
+Les photographies magnifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans un voyage à travers des recettes et des histoires qui mettent en avant la beauté de la simplicité et de la convivialité.
+
+Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres chers.
+
+'The Kinfolk Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain que ce livre trouvera une place spéciale dans le cœur de tout amoureux de la cuisine et des rencontres inspirantes.
+                        </textarea>
+                        <label for="available" class="form-label">Disponibilité</label>
+                        <select id="available" name="available" class="form-input form-input-update">
+                            <option value="true">disponible</option>
+                            <option value="false">non dispo.</option>
+                        </select>
+                        <input type="submit" id="validate" value="Valider" class="cta cta-input">
+                    </form>
+                </div>
+            </section>
+        </main>
+        <footer class="page-footer text-light">
+            <ul>
+                <li><a href="#">Politique de confidentialité</a></li>
+                <li><a href="#">Mentions légales</a></li>
+                <li>Tom Troc©</li>
+                <li><img src="images/tt-logo.svg" alt="Logo TT"></li>
+            </ul>
+        </footer>
+    </body>
+</html>
