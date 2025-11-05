b942e1e0046cd47afa83813a66cad284fc05392a
David Horès <hores.d@gmail.com>
2025-11-05 15:34:09 +0100

    Update signup template and style to display database content

--- app/Views/templates/signup.php
+++ app/Views/templates/signup.php
@@ -1,14 +1,17 @@
 <section class="section-auth">
     <div class="section-auth-wrapper">
         <h1 class="title">Inscription</h1>
-        <form aria-label="Formulaire d'inscription">
-            <label for="username" class="form-label">Pseudo</label>
-            <input type="text" id="username" name="username" class="form-input" required>
+        <?php if (!empty($error)): ?>
+            <p role="alert" class="text-error"><?= htmlspecialchars($error) ?></p>
+        <?php endif; ?>
+        <form action="index.php?action=register" method="post" aria-label="Formulaire d'inscription">
+            <label for="nickname" class="form-label">Pseudo</label>
+            <input type="text" id="nickname" name="nickname" value="<?= isset($nickname) ? htmlspecialchars($nickname) : '' ?>" class="form-input" required>
             <label for="email" class="form-label">Adresse email</label>
-            <input type="email" id="email" name="email" class="form-input" required>
+            <input type="email" id="email" name="email" value="<?= isset($email) ? htmlspecialchars($email) : '' ?>" class="form-input" required>
             <label for="password" class="form-label">Mot de passe</label>
             <input type="password" id="password" name="password" class="form-input" required>
-            <input type="submit" id="signup" value="S'inscrire" class="cta cta-input">
+            <input type="submit" id="register" value="S'inscrire" class="cta cta-input">
         </form>
         <p class="section-auth-text">
             Déjà inscrit(e) ?
