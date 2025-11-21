16f0078f7f837f786c6c0b8f9188254670907e19
David Horès <hores.d@gmail.com>
2025-11-21 17:51:14 +0100

    Create error template and add error section to global style and media query in style.css

---
+++ app/Views/templates/error.php
@@ -0,0 +1,5 @@
+<section class="section-error">
+    <h1 class="title">Erreur <?= htmlspecialchars($code) ?></h1>
+    <p>La page demandée est introuvable.</p>
+    <a href="index.php" class="cta">Retour</a>
+</section>
