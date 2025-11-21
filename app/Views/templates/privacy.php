09b93b3d18419cd0a1789fcdbf3b2bb409a1e814
David Horès <hores.d@gmail.com>
2025-11-21 18:44:08 +0100

    Create privacy and legal templates and add privacy and legal sections to global style and media query in style.css

---
+++ app/Views/templates/privacy.php
@@ -0,0 +1,6 @@
+<section class="section-privacy">
+    <h1 class="title">Politique de confidentialité</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consequat, quam vel euismod tempus, sem tortor molestie nunc, vel vulputate neque justo at purus. Morbi consequat tincidunt luctus.</p>
+    <p>Proin auctor ac tellus a ornare. Vestibulum tristique lacinia malesuada. Aenean sollicitudin maximus tincidunt. Donec sed dolor mattis, fermentum purus ut, tincidunt ipsum. Duis consectetur fermentum ante, nec consectetur tellus consequat aliquet. Nulla at orci a lectus sollicitudin vestibulum.</p>
+    <p>Suspendisse iaculis imperdiet ipsum, ut facilisis nunc pulvinar quis. Vivamus quis sollicitudin nisi, nec ultrices ipsum. Cras dignissim tincidunt magna. Etiam ut erat non mi pulvinar sollicitudin. Cras id mattis lorem. Nunc sed sodales velit. Duis facilisis justo felis, et mollis nisl blandit eget porttitor.</p>
+</section>
